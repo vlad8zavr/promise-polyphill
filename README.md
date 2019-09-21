7852c9bb99f88bd82b82799eebe5aea34e134292
Vladzavr <vlad8zavr@gmail.com>
2019-09-21 18:42:32 +0300

    added description

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # promise-polyphill
+
+**Что сделано**
+
+Реализована функиция, имитирующая действия промисов для простых случаев.
+
+1. Цепочка промисов, в ходе которой изначальное число удваивается несколько раз
+
+```javascript
+const promise = new Vlad8zavrPromise(function(resolve, reject) {
+    setTimeout(function() {
+
+        // set a 50% chance of success
+        let isSuccess = Math.floor(Math.random() * 10) > 4;
+        if (isSuccess) resolve(2);
+        else reject('error message in 1-st promise');
+
+    }, 1000) 
+})
+
+promise
+    .then(function(num) { console.log(num); return num * 2; })
+    .then(function(num) { console.log(num); return num * 2; })
+    .then(function(num) { console.log(num); })
+    .catch(function() {console.log('ERROR HAPPEND')})
+
+```
+
+2. Данный случай показывает, что `resolve()` не будет выполняться после того, как выполнился `reject()`. Верно и обратное.
+
+```javascript
+const promise = new Vlad8zavrPromise(function(resolve, reject) {
+    setTimeout(function() {
+
+        setTimeout(function() { resolve(2); }, 1000);
+        setTimeout(function() { reject('error message'); }, 500);
+
+    }, 1000) 
+})
+
+promise
+    .then(function(num) { console.log(num); return num + 2; })
+    .then(function(num) { console.log(num); return num + 2; })
+    .then(function(num) { console.log(num); })
+    .catch(function() {console.log('ERROR HAPPEND')})
+
+```
+
+**Что не удалось сделать**
+
+1. Данная реализация не отработает, если в одном из вызовов метода `then()` в колбэке вернуть новый промис вместо примитива.
+
+```javascript
+    .then(function(num) { 
+        return new Vlad8zavrPromise(function (resolve, reject) { resolve(5) })
+    })
+```
+
+2. Метод `catch()` не отлавливает ошибки `throw new Error()`
--- vlad8zavrPromise.js
+++ vlad8zavrPromise.js
@@ -3,91 +3,75 @@
 function Vlad8zavrPromise(callback) {
 
     this.thenChain = [];
+    this.caught = null;
     this.isActiveProcess = false;
-    this.currentResult = null;
 
     function resolve(data) {
-        console.log('[RESOLVE]');
-        console.log(data);
-        console.log('----------')
+
         if (!this.isActiveProcess) {
             this.isActiveProcess = true;
 
-            console.log('length', this.thenChain.length);
-
-            this.currentResult = null;
-            if (!this.thenChain.length) this.currentResult = data;
-
             this.thenChain.forEach(function(callbackItem) {
-                console.log('DATA');
                 data = callbackItem(data);
             })
         }
     }
     function reject(error) { 
-        if (!this.isActiveProcess) {
+        if (!this.isActiveProcess && this.caught) {
             this.isActiveProcess = true;
-            console.error('[reject]', error); 
+            console.log('[reject]', error);
+            this.caught(error);
         }
     }
 
-    console.log('callback', callback);
     callback(resolve.bind(this), reject.bind(this));
 
     this.then = function(callbackThen) {
-
-        console.log('[THEN]');
-        console.log(callbackThen);
-
 
         // передача выполнения callbackThen в resolve()
         this.thenChain.push(callbackThen);
 
         // then returns promise
         return this;
-
     }
     
-    this.catch = function() {}
-
-    //console.log( 'callback result', callback(resolve.bind(this), reject.bind(this)) );
-    if (this.currentResult) {
-        console.log('this.currentResult', this.currentResult);
-        return this.currentResult;
+    this.catch = function(callBackCatch) {
+        this.caught = callBackCatch;
     }
+
 }
 
 const promise = new Vlad8zavrPromise(function(resolve, reject) {
     setTimeout(function() {
 
-        let isSuccess = Math.floor(Math.random() * 10) > 4;
-        if (isSuccess) resolve(2);
-        else reject('error message in 1-st promise');
+        // let isSuccess = Math.floor(Math.random() * 10) > 4;
+        // if (isSuccess) resolve(2);
+        // else reject('error message in 1-st promise');
 
-        // setTimeout(function() { resolve(2); }, 1000);
-        // setTimeout(function() { reject('error message'); }, 500);
+        setTimeout(function() { resolve(2); }, 1000);
+        setTimeout(function() { reject('error message'); }, 500);
 
-    }, 1000)
+    }, 1000) 
 })
 
-// promise
-//     .then(function(num) { console.log(num); return num + 2; })
-//     .then(function(num) { console.log(num); return num + 2; })
-//     .then(function(num) { console.log(num); })
-
-
 promise
-    .then(function(num) {return num + 2; })
-    .then(function(num) { 
-        //return num + 2;
-        let prm = new Vlad8zavrPromise(function (resolve, reject) { resolve(137) })
-        console.log('prm', prm);
-        console.log('prm()', new Vlad8zavrPromise(function (resolve, reject) { resolve(137) }));
-        return prm;
-        //return new Vlad8zavrPromise(function (resolve, reject) { resolve(137) })
-    })
-    .then(function(num) { return num + 2; })
-    .then(function(num) { console.log('res', num); })
+    .then(function(num) { console.log(num); return num + 2; })
+    .then(function(num) { console.log(num); return num + 2; })
+    .then(function(num) { console.log(num); })
+    .catch(function() {console.log('ERROR HAPPEND')})
+
+
+// promise
+//     .then(function(num) {return num + 2; })
+//     .then(function(num) { 
+//         //return num + 2;
+//         let prm = new Vlad8zavrPromise(function (resolve, reject) { resolve(137) })
+//         console.log('prm', prm);
+//         return prm;
+//         //return new Vlad8zavrPromise(function (resolve, reject) { resolve(137) })
+//     })
+//     .then(function(num) { return num + 2; })
+//     .then(function(num) { console.log('res', num); })
  
 
 

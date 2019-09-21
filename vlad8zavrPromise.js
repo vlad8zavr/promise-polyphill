6b3183fa238c62d8f4ee0757c4f5d25b50f9cca2
Vladzavr <vlad8zavr@gmail.com>
2019-09-21 11:17:11 +0300

    on my way to get then()

--- vlad8zavrPromise.js
+++ vlad8zavrPromise.js
@@ -5,44 +5,31 @@ function Vlad8zavrPromise(callback) {
     this.resolve = function() { console.log('resolve'); }
     this.reject  = function() { console.log('reject'); }
 
+    //callback(this.resolve.bind(this), this.reject.bind(this));
     callback(this.resolve, this.reject);
 
     this.then = function(callbackThen) {
 
         callbackThen();
         return this;
-        
-        // return new Vlad8zavrPromise((resolve, reject) => {
-        //     callbackThen();
-        // })
+
     }
+    
     this.catch = function() {}
 
 }
 
 let promise = new Vlad8zavrPromise((resolve, reject) => {
     console.log('----- promise -----');
-    resolve();
+    resolve(1);
     reject();
     console.log('-------------------');
 })
 
 promise
-    .then(() => {
-        return new Vlad8zavrPromise((resolve, reject) => {
-            setTimeout(() => {
-                console.log('THEN OPTION 1');
-            }, 1000)
-        })
-    })
-    .then(() => {
-        return new Vlad8zavrPromise((resolve, reject) => {
-            setTimeout(() => {
-                console.log('THEN OPTION 2');
-            }, 500)
-        })
-    })
-
+    .then(number => number + 2)
+    .then(number => number + 2)
+    .then(number => console.log(number))
 
 // promise
 //     .then(() => {
@@ -52,4 +39,14 @@ promise
 //             }, 1000)
 //         })
 //     })
+//     .then(() => {
+//         return new Vlad8zavrPromise((resolve, reject) => {
+//             setTimeout(() => {
+//                 console.log('THEN OPTION 2');
+//             }, 500)
+//         })
+//     })
+
+
+
 

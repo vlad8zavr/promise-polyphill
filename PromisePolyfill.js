1ebd6fd2f6772d074f90068658dfded131787bd7
Vladzavr <vlad8zavr@gmail.com>
2019-09-23 00:40:13 +0300

    examples commented

--- PromisePolyfill.js
+++ PromisePolyfill.js
@@ -104,18 +104,18 @@
 
 
 
-const promise = new Promise(function(resolve, reject) {
-    setTimeout(function() {
+// const promise = new Promise(function(resolve, reject) {
+//     setTimeout(function() {
 
-        let isSuccess = Math.floor(Math.random() * 10) > 4;
-        if (isSuccess) resolve(2);
-        else reject('error message in 1-st promise');
+//         let isSuccess = Math.floor(Math.random() * 10) > 4;
+//         if (isSuccess) resolve(2);
+//         else reject('error message in 1-st promise');
 
-        //setTimeout(function() { resolve(2); }, 1000);
-        //setTimeout(function() { reject('error message'); }, 500);
+//         //setTimeout(function() { resolve(2); }, 1000);
+//         //setTimeout(function() { reject('error message'); }, 500);
 
-    }, 1000) 
-})
+//     }, 1000) 
+// })
 
 // promise
 //     .then(function(num) { console.log(num); return num + 2; })
@@ -124,14 +124,14 @@ const promise = new Promise(function(resolve, reject) {
 //     .catch(function() {console.log('ERROR HAPPEND')})
 
 
-promise
-    .then(function(num) {return num + 2; })
-    .then(function(num) { 
-        //return num + 2;
-        return new Promise(function (resolve, reject) { resolve(137) })
-    })
-    .then(function(num) { return num + 2; })
-    .then(function(num) { console.log('res', num); })
+// promise
+//     .then(function(num) {return num + 2; })
+//     .then(function(num) { 
+//         //return num + 2;
+//         return new Promise(function (resolve, reject) { resolve(137) })
+//     })
+//     .then(function(num) { return num + 2; })
+//     .then(function(num) { console.log('res', num); })
  
 
 
